e9c80908c753a7aaad36fa4a5001ee370fff82c3
karshdev <sharmaaakarsh332@gmail.com>
2025-01-15 00:09:39 +0530

    added rows

--- src/User List/UserList.jsx
+++ src/User List/UserList.jsx
@@ -39,6 +39,159 @@ const UserList = () => {
               <button className="bg-gray-500 text-white px-4 py-1 rounded hover:bg-gray-600">Yes</button>
             </td>
           </tr>
+          <tr className="hover:bg-gray-50">
+            <td className="border border-gray-300 p-2 text-center">12/03/25</td>
+            <td className="border border-gray-300 p-2 text-center">2233223322</td>
+            <td className="border border-gray-300 p-2 text-center">Jhon</td>
+            <td className="border border-gray-300 p-2 text-center">Doe</td>
+            <td className="border border-gray-300 p-2 text-center">W2 4EB</td>
+            <td className="border border-gray-300 p-2 text-center">10 Smith Road London</td>
+            <td className="border border-gray-300 p-2 text-center">9876543210</td>
+            <td className="border border-gray-300 p-2 text-center">bishtechnologies@yopmail.com</td>
+            <td className="border border-gray-300 p-2 text-center">
+              <button className="bg-blue-500 text-white px-4 py-1 rounded hover:bg-blue-600">Quote</button>
+            </td>
+            <td className="border border-gray-300 p-2 text-center">
+              <button className="bg-green-500 text-white px-4 py-1 rounded hover:bg-green-600">Book</button>
+            </td>
+            <td className="border border-gray-300 p-2 text-center">
+              <button className="bg-gray-500 text-white px-4 py-1 rounded hover:bg-gray-600">Yes</button>
+            </td>
+          </tr>
+          <tr className="hover:bg-gray-50">
+            <td className="border border-gray-300 p-2 text-center">12/03/25</td>
+            <td className="border border-gray-300 p-2 text-center">2233223322</td>
+            <td className="border border-gray-300 p-2 text-center">Jhon</td>
+            <td className="border border-gray-300 p-2 text-center">Doe</td>
+            <td className="border border-gray-300 p-2 text-center">W2 4EB</td>
+            <td className="border border-gray-300 p-2 text-center">10 Smith Road London</td>
+            <td className="border border-gray-300 p-2 text-center">9876543210</td>
+            <td className="border border-gray-300 p-2 text-center">bishtechnologies@yopmail.com</td>
+            <td className="border border-gray-300 p-2 text-center">
+              <button className="bg-blue-500 text-white px-4 py-1 rounded hover:bg-blue-600">Quote</button>
+            </td>
+            <td className="border border-gray-300 p-2 text-center">
+              <button className="bg-green-500 text-white px-4 py-1 rounded hover:bg-green-600">Book</button>
+            </td>
+            <td className="border border-gray-300 p-2 text-center">
+              <button className="bg-gray-500 text-white px-4 py-1 rounded hover:bg-gray-600">Yes</button>
+            </td>
+          </tr>
+          <tr className="hover:bg-gray-50">
+            <td className="border border-gray-300 p-2 text-center">12/03/25</td>
+            <td className="border border-gray-300 p-2 text-center">2233223322</td>
+            <td className="border border-gray-300 p-2 text-center">Jhon</td>
+            <td className="border border-gray-300 p-2 text-center">Doe</td>
+            <td className="border border-gray-300 p-2 text-center">W2 4EB</td>
+            <td className="border border-gray-300 p-2 text-center">10 Smith Road London</td>
+            <td className="border border-gray-300 p-2 text-center">9876543210</td>
+            <td className="border border-gray-300 p-2 text-center">bishtechnologies@yopmail.com</td>
+            <td className="border border-gray-300 p-2 text-center">
+              <button className="bg-blue-500 text-white px-4 py-1 rounded hover:bg-blue-600">Quote</button>
+            </td>
+            <td className="border border-gray-300 p-2 text-center">
+              <button className="bg-green-500 text-white px-4 py-1 rounded hover:bg-green-600">Book</button>
+            </td>
+            <td className="border border-gray-300 p-2 text-center">
+              <button className="bg-gray-500 text-white px-4 py-1 rounded hover:bg-gray-600">Yes</button>
+            </td>
+          </tr>
+          <tr className="hover:bg-gray-50">
+            <td className="border border-gray-300 p-2 text-center">12/03/25</td>
+            <td className="border border-gray-300 p-2 text-center">2233223322</td>
+            <td className="border border-gray-300 p-2 text-center">Jhon</td>
+            <td className="border border-gray-300 p-2 text-center">Doe</td>
+            <td className="border border-gray-300 p-2 text-center">W2 4EB</td>
+            <td className="border border-gray-300 p-2 text-center">10 Smith Road London</td>
+            <td className="border border-gray-300 p-2 text-center">9876543210</td>
+            <td className="border border-gray-300 p-2 text-center">bishtechnologies@yopmail.com</td>
+            <td className="border border-gray-300 p-2 text-center">
+              <button className="bg-blue-500 text-white px-4 py-1 rounded hover:bg-blue-600">Quote</button>
+            </td>
+            <td className="border border-gray-300 p-2 text-center">
+              <button className="bg-green-500 text-white px-4 py-1 rounded hover:bg-green-600">Book</button>
+            </td>
+            <td className="border border-gray-300 p-2 text-center">
+              <button className="bg-gray-500 text-white px-4 py-1 rounded hover:bg-gray-600">Yes</button>
+            </td>
+          </tr>
+          <tr className="hover:bg-gray-50">
+            <td className="border border-gray-300 p-2 text-center">12/03/25</td>
+            <td className="border border-gray-300 p-2 text-center">2233223322</td>
+            <td className="border border-gray-300 p-2 text-center">Jhon</td>
+            <td className="border border-gray-300 p-2 text-center">Doe</td>
+            <td className="border border-gray-300 p-2 text-center">W2 4EB</td>
+            <td className="border border-gray-300 p-2 text-center">10 Smith Road London</td>
+            <td className="border border-gray-300 p-2 text-center">9876543210</td>
+            <td className="border border-gray-300 p-2 text-center">bishtechnologies@yopmail.com</td>
+            <td className="border border-gray-300 p-2 text-center">
+              <button className="bg-blue-500 text-white px-4 py-1 rounded hover:bg-blue-600">Quote</button>
+            </td>
+            <td className="border border-gray-300 p-2 text-center">
+              <button className="bg-green-500 text-white px-4 py-1 rounded hover:bg-green-600">Book</button>
+            </td>
+            <td className="border border-gray-300 p-2 text-center">
+              <button className="bg-gray-500 text-white px-4 py-1 rounded hover:bg-gray-600">Yes</button>
+            </td>
+          </tr>
+          <tr className="hover:bg-gray-50">
+            <td className="border border-gray-300 p-2 text-center">12/03/25</td>
+            <td className="border border-gray-300 p-2 text-center">2233223322</td>
+            <td className="border border-gray-300 p-2 text-center">Jhon</td>
+            <td className="border border-gray-300 p-2 text-center">Doe</td>
+            <td className="border border-gray-300 p-2 text-center">W2 4EB</td>
+            <td className="border border-gray-300 p-2 text-center">10 Smith Road London</td>
+            <td className="border border-gray-300 p-2 text-center">9876543210</td>
+            <td className="border border-gray-300 p-2 text-center">bishtechnologies@yopmail.com</td>
+            <td className="border border-gray-300 p-2 text-center">
+              <button className="bg-blue-500 text-white px-4 py-1 rounded hover:bg-blue-600">Quote</button>
+            </td>
+            <td className="border border-gray-300 p-2 text-center">
+              <button className="bg-green-500 text-white px-4 py-1 rounded hover:bg-green-600">Book</button>
+            </td>
+            <td className="border border-gray-300 p-2 text-center">
+              <button className="bg-gray-500 text-white px-4 py-1 rounded hover:bg-gray-600">Yes</button>
+            </td>
+          </tr>
+          <tr className="hover:bg-gray-50">
+            <td className="border border-gray-300 p-2 text-center">12/03/25</td>
+            <td className="border border-gray-300 p-2 text-center">2233223322</td>
+            <td className="border border-gray-300 p-2 text-center">Jhon</td>
+            <td className="border border-gray-300 p-2 text-center">Doe</td>
+            <td className="border border-gray-300 p-2 text-center">W2 4EB</td>
+            <td className="border border-gray-300 p-2 text-center">10 Smith Road London</td>
+            <td className="border border-gray-300 p-2 text-center">9876543210</td>
+            <td className="border border-gray-300 p-2 text-center">bishtechnologies@yopmail.com</td>
+            <td className="border border-gray-300 p-2 text-center">
+              <button className="bg-blue-500 text-white px-4 py-1 rounded hover:bg-blue-600">Quote</button>
+            </td>
+            <td className="border border-gray-300 p-2 text-center">
+              <button className="bg-green-500 text-white px-4 py-1 rounded hover:bg-green-600">Book</button>
+            </td>
+            <td className="border border-gray-300 p-2 text-center">
+              <button className="bg-gray-500 text-white px-4 py-1 rounded hover:bg-gray-600">Yes</button>
+            </td>
+          </tr>
+
+          <tr className="hover:bg-gray-50">
+            <td className="border border-gray-300 p-2 text-center">12/03/25</td>
+            <td className="border border-gray-300 p-2 text-center">2233223322</td>
+            <td className="border border-gray-300 p-2 text-center">Jhon</td>
+            <td className="border border-gray-300 p-2 text-center">Doe</td>
+            <td className="border border-gray-300 p-2 text-center">W2 4EB</td>
+            <td className="border border-gray-300 p-2 text-center">10 Smith Road London</td>
+            <td className="border border-gray-300 p-2 text-center">9876543210</td>
+            <td className="border border-gray-300 p-2 text-center">bishtechnologies@yopmail.com</td>
+            <td className="border border-gray-300 p-2 text-center">
+              <button className="bg-blue-500 text-white px-4 py-1 rounded hover:bg-blue-600">Quote</button>
+            </td>
+            <td className="border border-gray-300 p-2 text-center">
+              <button className="bg-green-500 text-white px-4 py-1 rounded hover:bg-green-600">Book</button>
+            </td>
+            <td className="border border-gray-300 p-2 text-center">
+              <button className="bg-gray-500 text-white px-4 py-1 rounded hover:bg-gray-600">Yes</button>
+            </td>
+          </tr>
         </tbody>
       </table>
     </div>
